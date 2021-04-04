2f9927c4599fd1dc8136cf8c1fd7603b15265867
Bhargavi Saikia <bhargavisaikia187@gmail.com>
2021-04-04 23:58:21 +0530

    test basic logic of the program

--- Rock-Paper-Scissors_Game.py
+++ Rock-Paper-Scissors_Game.py
@@ -1,6 +1,17 @@
 import random
+
 def play():
     user=input("Please enter your choice? 'r' for rock, 'p' for paper and 's' for scissor\n")
     computer=random.choice(['r','p','s'])
     print(computer)
-play()
+
+    if user==computer:
+        return "It\'s a tie!"
+    else:
+        if((user=='r' and computer=='s') or (user=='s' and computer=='p') or (user=='p' and computer=='r')):
+            return "you won"
+        else:
+            return "you lost"
+
+
+print(play())
